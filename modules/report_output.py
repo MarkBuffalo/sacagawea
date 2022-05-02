09faa1d53dd4cba10b6fa98465c42ac9ce9b72e4
MarkBuffalo <phil@grulo.com>
2022-05-02 11:17:22 -0500

    broken report

--- modules/report_output.py
+++ modules/report_output.py
@@ -1,5 +1,24 @@
-# This will be developed very soon.
+import glob
+from collections import defaultdict
+from pathlib import Path
 
 class ReportOutput:
     def __init__(self):
-        pass
+        self.img_dict = defaultdict(list)
+
+    @staticmethod
+    def get_host_info(image_name):
+        return Path(image_name).stem
+
+    def load_report_info(self):
+        images = glob.glob("../shots/*.png")
+
+        new_html = ""
+
+        for img in images:
+            file_name = self.get_host_info(img)
+            host = file_name.split("_")[0]
+            port = file_name.split("_")[1]
+
+            self.img_dict[host].append(port)
+
